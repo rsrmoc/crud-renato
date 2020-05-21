552bb4a52cc80e828f814a56fe98355623ece606
renato <rsrmoc@gmail.com>
2020-05-20 21:26:41 -0300

    iyuiyui

--- src/app/components/users/user.service.ts
+++ src/app/components/users/user.service.ts
@@ -25,25 +25,22 @@ export class UserService {
     const _url = `${this.url}/user/${id}`;
     return this.http.get<User>(_url);
   }
-
  
-
   updateUser(id: string, request: any){
-
     const _url_up = `${this.url}/CriarUser/${id}`;
-
     return this.http.put<User>(_url_up, JSON.stringify(request));
-
+  }
+ 
+  createUser(request: User): Observable<User> {
+    const _url_up = `${this.url}/CriarUser`;
+    return this.http.post<User>(_url_up,request);
   }
 
-  
-
-  createUser(request: User): Observable<User> {
-
-    const _url_up = `${this.url}/CriarUser`;
-
-    return this.http.post<User>(_url_up,request);
-    
+  delete(id: number): Observable<User> {
+    const url = `${this.url}/DelUser/${id}`;
+    return this.http.delete<User>(url).pipe(
+     
+    );
   }
 
 }
